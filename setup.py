282344543b09e971eb7197dd9255b8e47573cd50
J.C. Páez R. <91022556+paez-dev@users.noreply.github.com>
2025-05-23 00:00:02 +0000

    Actualización del setup

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="proyecto_integrado_v_aval_tracker",
-    version="0.0.1",
+    version="0.0.2",
     author="Jean Carlos Páez Ramírez",
     author_email="",
     description="Paquete para descargas de datos del grupo AVAL de Yahoo Finance",
-    py_modules=["actividad_1"],
+    py_modules=["actividad_2"],
     packages=find_packages(),  # Detecta automáticamente los paquetes
     install_requires=[
         "pandas>=2.2.3",  # Necesario para manipulación de datos
+        'numpy',
+        'scikit-learn',
+        'streamlit',
+        'plotly',
+        "joblib",
         "yfinance>=0.1.64",  # Necesario para descargar datos de Yahoo Finance
     ],
     python_requires=">=3.6",  # Indica la versión mínima de Python
